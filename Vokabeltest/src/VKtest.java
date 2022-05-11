b2c01131e87db6eaa196412aaa8e2542000f7b33
LeonasOnGitHub <103933123+LeonasOnGitHub@users.noreply.github.com>
2022-05-11 11:20:38 +0200

    Add files via upload

---
+++ Vokabeltest/src/VKtest.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class VKtest {
+  
+  public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.println();       
+    System.out.println("Gib immer zu erst das Wort und dann Die übersetzung ein");
+    System.out.println("Wenn du Fertig bist gib P ein");
+    System.out.println();
+    ArrayList<String> wort = new ArrayList<String>();
+    ArrayList<String> wort2 = new ArrayList<String>();
+    
+    for (int i=0 ;-1<i ;i++ ) {
+    	String a=sc.next();
+    	if (a.equals("P")) {
+    		break;
+    	}
+    	String b=sc.next();
+      wort.add(a);
+      wort2.add(b);
+      System.out.println();
+    } // end of for
+    System.out.println();
+    System.out.println();
+    for (int x=0 ;x+1>x ;x++ ) {
+      int h=  (int) (Math.random() *wort.size());
+      int r=  (int) (Math.random() *2);
+      if (r>0) {
+      System.out.println(" " +wort.get(h));
+      System.out.println(" Lousung:");
+      String q=sc.next();
+      System.out.println(" " +wort2.get(h));
+      System.out.println();
+      }else {
+   	  System.out.println(" " +wort2.get(h)); 
+   	  System.out.println(" Lousung:");
+   	  String q=sc.next();
+      System.out.println(" " +wort.get(h));
+      System.out.println();
+      }
+      } // end of for
+  } // end of main
+
+} // end of class VokabelTester
---
+++ Vokabeltest/src/test.java
@@ -0,0 +1,10 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r=  (int) (Math.random() *2);
+		System.out.println(r);
+	}
+
+}
